511ceb1661e6e7009f1f4e668567a45ff424422b
Xavier Arias <xavier_rate@outlook.com>
2021-10-14 01:03:45 +1100

    TMP and Started a main menu navigation!

--- Assets/NullFrameworkException/SortingAlgorithm/Storters/BubbleComparable.cs
+++ Assets/NullFrameworkException/SortingAlgorithm/Storters/BubbleComparable.cs
@@ -10,26 +10,31 @@ namespace Sorting
     {
         [SerializeField] private int[] array;
 
-        private void Update()
+        private void Awake()
         {
-            string logOutput = "";
-            BubbleSorter(array);
-            foreach (var arr in array)
+            string logOutput = ""; // We initialize the string
+            BubbleSorter(array); // We sort the array
+            foreach (var arr in array) // We find each sorted value in the array
             {
-                logOutput += $"{arr.ToString()} ,";
+                logOutput += $"{arr.ToString()} ,"; // We string it!
             }
-            Debug.Log(logOutput);
+            Debug.Log(logOutput); // We display it inside the unity console!
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input">Whatever value is that the user has inputted</param>
+        /// <typeparam name="T"> T is the generic template for the comparer function </typeparam>
         public static void BubbleSorter<T>(T[] input) where T : IComparable<T>
         {
-            for (int x = 0; x < input.Length; x++)
+            for (int x = 0; x < input.Length; x++) // Getting the array
             {
-                for (int y = 0; y < input.Length - 1; y++)
+                for (int y = 0; y < input.Length - 1; y++) // Getting the array for comparison
                 {
-                    if (input[y].CompareTo(input[y + 1]) > 0)
+                    if (input[y].CompareTo(input[y + 1]) > 0) // We are comparing the values
                     {
-                        if (input[y].CompareTo(input[y + 1]) > 0)
+                        if (input[y].CompareTo(input[y + 1]) > 0)  // Doing it again!
                         {
                             T temp = input[y + 1];
                             input[y + 1] = input[y];
